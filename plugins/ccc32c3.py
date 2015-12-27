a42124c95ef9b268b1be85ed7c8b54162064e9db
Maik Kulbe <info@linux-web-development.de>
2015-12-27 23:12:15 +0100

    Removed unused imports

--- plugins/ccc32c3.py
+++ plugins/ccc32c3.py
@@ -7,11 +7,9 @@ from time import time
 
 from plugins.plugin import Plugin
 
-from bytebot_config import BYTEBOT_HTTP_TIMEOUT, BYTEBOT_HTTP_MAXSIZE
-from bytebot_config import BYTEBOT_PLUGIN_CONFIG
-
 with open("data/events_32c3.json") as json_file:
     json_data = json.load(json_file)
+
 
 class ccc32c3(Plugin):
 
